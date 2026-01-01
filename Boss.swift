129c71027347e96d014eb9305c7df505cfe7bee9
Ura-31bit <f7ccvj6yjs@privaterelay.appleid.com>
2026-01-01 10:21:38 +0900

    Refactor Boss struct to include respawn and notify times

--- Boss.swift
+++ Boss.swift
@@ -1,4 +1,13 @@
-struct Boss {
+import Foundation
+struct Boss: Identifiable {
+   let id = UUID()
    let name: String
-   let notifySeconds: TimeInterval
+   let respawnSeconds: TimeInterval
+   let notifyBeforeSeconds: TimeInterval
+   var notifyAfterSeconds: TimeInterval {
+       respawnSeconds - notifyBeforeSeconds
+   }
+   var notifyMessage: String {
+       "\(name)のリポップ\(Int(notifyBeforeSeconds))秒前です。"
+   }
 }
